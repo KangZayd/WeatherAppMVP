ceb8e94e578cc8d326dbe5376fd226f140887126
Saghayam Nadar <saghayam.nadar@Infraseepz.lan>
2018-09-01 15:24:24 +0530

    ll the screens design done

---
+++ app/src/main/java/test/gojek/gojektest/ui/App.kt
@@ -0,0 +1,10 @@
+package test.gojek.gojektest.ui
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/test/gojek/gojektest/ui/base/BaseActivity.kt
@@ -0,0 +1,8 @@
+package test.gojek.gojektest.ui.base
+
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity(){
+
+
+}
