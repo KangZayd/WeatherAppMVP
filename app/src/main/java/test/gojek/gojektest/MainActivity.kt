dbd12a0a9dd115a2f4d8eadd8b888b3dbdf5415b
Saghayam Nadar <saghayam.nadar@Infraseepz.lan>
2018-09-01 10:42:34 +0530

    Initial commit

---
+++ app/src/main/java/test/gojek/gojektest/MainActivity.kt
@@ -0,0 +1,15 @@
+package test.gojek.gojektest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+//c703792f9a4540e595650713180109
+//https://api.apixu.com/v1/current.json?key=c703792f9a4540e595650713180109&q=Paris
+//http://api.apixu.com/v1/forecast.json?key=c703792f9a4540e595650713180109&q=Paris
